9a24e549636d2e73329849e2a4cf74be758aad2b
PratibhaCD <pratibhasharma@countrydelight.in>
2024-07-19 11:18:39 +0530

    Lib code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,11 @@
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id ("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 android {
@@ -49,8 +54,92 @@ android {
     }
 }
 
-dependencies {
+mavenPublishing{
+    configure(
+        AndroidSingleVariantLibrary(
+        // the published variant
+        variant = "release",
+        // whether to publish a sources jar
+        sourcesJar = true,
+        // whether to publish a javadoc jar
+        publishJavadocJar = true,
+    )
+    )
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
 
+    coordinates("com.github.PratibhaCD", "TestMaven", "1.0.2")
+
+    pom {
+        name.set("TestMavenLib")
+        description.set("A description of what my library does.")
+        inceptionYear.set("2024")
+        url.set("https://github.com/PratibhaCD/TestMaven")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("PratibhaCD")
+                name.set("PratibhaCD")
+                url.set("https://github.com/username/")
+            }
+        }
+        scm {
+            connection.set("scm:git:git://github.com/PratibhaCD/TestMaven.git")
+            developerConnection.set("scm:git:ssh://github.com:PratibhaCD/TestMaven.git")
+            url.set("https://github.com/PratibhaCD/TestMaven")
+        }
+    }
+
+    signAllPublications()
+}
+
+//publishing {
+//    publications {
+//        create<MavenPublication>("mavenCentral") {
+////            from components.java
+//
+//            groupId = "com.github.PratibhaCD"
+//            artifactId = "TestMaven"
+//            version = "1.0.0"
+//
+//            pom {
+//                name = "TestMaven"
+//                description = "Test maven central project"
+//                url = "https://github.com/PratibhaCD/TestMaven"
+//
+//                licenses {
+//                    license {
+//                        name = "The Apache License, Version 2.0"
+//                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+//                    }
+//                }
+//
+//                developers {
+//                    developer {
+//                        id = "PratibhaCD"
+//                        name = "PratibhaCD"
+//                        email = "pratibhasharma@countrydelight.in"
+//                    }
+//                }
+//
+//                scm {
+//                    connection.set("scm:git:git://github.com/PratibhaCD/TestMaven.git")
+//                    developerConnection.set("scm:git:ssh://github.com:PratibhaCD/TestMaven.git")
+//                    url.set("https://github.com/PratibhaCD/TestMaven")
+//                }
+//            }
+//        }
+//    }
+//}
+
+
+dependencies {
+    implementation(project(":TestAppLib"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
